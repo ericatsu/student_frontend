88d80f9c4aacfb6b219de39235fff9c4951209be
ericatsu <ekaatsu@st.ug.edu.gh>
2023-09-27 03:34:16 +0000

    Student Service

--- lib/service/student_services.dart
+++ lib/service/student_services.dart
@@ -1,77 +1,41 @@
-// import 'dart:convert';
+import 'dart:convert';
 
-// import 'package:http/http.dart' as http;
-// import 'package:student_frontend/shared/exports.dart';
+import 'package:http/http.dart' as http;
+import 'package:student_frontend/shared/exports.dart';
 
-// void fetchStudents() async {
-//   var response = await http.get(Uri.parse(url));
-//   if (response.statusCode == 200) {
-//     // Successful response
-//     List<dynamic> studentsData = jsonDecode(response.body);
+List<Student> students = [];
+Future<void> fetchStudents() async {
+  
+    String url = 'https://studentapi-production.up.railway.app/api/v1/student';
 
-//     // Process the student data
-//     // ...
-//   } else {
-//     // Error handling
-//     print('Failed to fetch students. Error code: ${response.statusCode}');
-//   }
-// }
+    var response = await http.get(Uri.parse(url));
 
-// //Create Student
-// void createStudent(String name, String email, String dob, int age) async {
-//   Map<String, dynamic> studentData = {
-//     'name': name,
-//     'email': email,
-//     'dob': dob,
-//     'age': age,
-//   };
+    if (response.statusCode == 200) {
+      List<dynamic> studentsData = jsonDecode(response.body);
 
-//   var response = await http.post(Uri.parse(url), body: studentData);
+      students = studentsData.map((json) => Student.fromJson(json)).toList();
 
-//   if (response.statusCode == 201) {
-//     // Successful response
-//     var createdStudent = jsonDecode(response.body);
+    } else {
+      print('Failed to fetch students. Error code: ${response.statusCode}');
+    }
 
-//     // Process the created student data
-//     // ...
-//   } else {
-//     // Error handling
-//     print('Failed to create student. Error code: ${response.statusCode}');
-//   }
-// }
+    
+  }
 
-// //Update an existing student
-// void updateStudent(
-//     int id, String name, String email, String dob, int age) async {
-//   Map<String, dynamic> studentData = {
-//     'name': name,
-//     'email': email,
-//     'dob': dob,
-//     'age': age,
-//   };
+  void deleteStudent(int studentId) async {
+    String apiUrl =
+        'https://studentapi-production.up.railway.app/api/v1/student/$studentId';
 
-//   var response = await http.put(Uri.parse('$url/$id'), body: studentData);
+    try {
+      final response = await http.delete(Uri.parse(apiUrl));
 
-//   if (response.statusCode == 200) {
-//     // Successful response
-//     var updatedStudent = jsonDecode(response.body);
-
-//     // Process the updated student data
-//     // ...
-//   } else {
-//     // Error handling
-//     print('Failed to update student. Error code: ${response.statusCode}');
-//   }
-// }
-
-// void deleteStudent(int id) async {
-//   var response = await http.delete(Uri.parse('$url/$id'));
-
-//   if (response.statusCode == 204) {
-//     // Successful response
-//     print('Student with ID $id has been deleted.');
-//   } else {
-//     // Error handling
-//     print('Failed to delete student. Error code: ${response.statusCode}');
-//   }
-// }
+      if (response.statusCode == 200) {
+        
+        fetchStudents();
+      } else {
+        print('Failed to delete student. Status code: ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Failed to delete student. Error: $error');
+    }
+  }
